94c45265a364daf52dff2be5c1f2ec8be404e038
Leandro Estevez <leoest.dev@gmail.com>
2021-07-15 22:25:41 -0400

    Set up. Create 3 main components

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        Catch
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">The</span>
+        </span>
+        Day
+      </h1>
+      <h3 className="tagline">
+        <span>Fresh Daily</span>
+      </h3>
+    </header>
+  );
+};
+
+export default Header;
