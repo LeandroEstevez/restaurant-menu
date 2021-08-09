6440d2936b9b1758209ffcb25d8fc376f348653b
Leandro Estevez <leoest.dev@gmail.com>
2021-08-08 20:21:09 -0400

    New components

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import "../App.css";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import Fish from "./Fish";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
@@ -15,14 +17,39 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = (key) => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="West is cool"></Header>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => {
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={addToOrder}
+              >
+                key
+              </Fish>;
+            })}
+          </ul>
         </div>
         <Order></Order>
-        <Inventory addFish={this.addFish}></Inventory>
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        ></Inventory>
       </div>
     );
   }
